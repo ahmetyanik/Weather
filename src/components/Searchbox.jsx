6e0aef506501569a1272a5a41830bef1f49c4818
ahmet yanik <ahmetyank4242@gmail.com>
2021-08-22 20:38:18 +0200

    Last commit

---
+++ src/components/Searchbox.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function Searchbox(){
+
+
+    return(
+        <div className="homepage-input">
+        <form action="/search" method="get">
+          <input name="city" placeholder="Please write your location" />
+          <div className="btn">
+            <button>Click</button>
+          </div>
+        </form>
+      </div>
+    )
+}
+
+
+export default Searchbox;
+
