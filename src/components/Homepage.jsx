bef47e30f8b84ce17af8cb4df25398353dd7c6b0
Yanik <ahmet.yanik@nteam.de>
2025-01-23 18:02:09 +0100

    first commit

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Searchbox from "./Searchbox";
+
+function Homepage() {
+
+  console.log("Hello World2");
+
+  return (
+    <div
+      className="homepage"
+      style={{
+        backgroundImage: "url(" + "../images/background.jpg" + ")",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+      }}
+    >
+  <Searchbox/>
+  
+    </div>
+  );
+}
+
+export default Homepage;
